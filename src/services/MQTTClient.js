4cd8cfa7bdee723e7e31d997110d1db14715fd1a
nguyendhst <mountngubinh@gmail.com>
2023-04-16 15:10:48 +0700

    mqtt

--- src/services/MQTTClient.js
+++ src/services/MQTTClient.js
@@ -4,8 +4,6 @@ import { AsyncStorage } from "react-native";
 const port = 1883;
 const securePort = 8883;
 const host = "io.adafruit.com";
-// const username = "Your Adafruit IO Username";
-// const password = "Your Adafruit IO Key";
 const client = new Paho.Client(host, port);
 
 client.onConnectionLost = (responseObject) => {
